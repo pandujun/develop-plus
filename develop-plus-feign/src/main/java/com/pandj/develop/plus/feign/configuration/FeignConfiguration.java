a23ddb95a51e813eb4dee3eaebf6446a286c232b
pandujun <1355445296@qq.com>
2025-02-28 14:36:54 +0800

    fix(feign): 去除feign的所有请求头参数

--- develop-plus-feign/src/main/java/com/pandj/develop/plus/feign/configuration/FeignConfiguration.java
+++ develop-plus-feign/src/main/java/com/pandj/develop/plus/feign/configuration/FeignConfiguration.java
@@ -36,14 +36,14 @@ public class FeignConfiguration {
             ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
             if (null != attributes) {
                 HttpServletRequest request = attributes.getRequest();
-                Map<String, String> headers = this.getRequestHeaders(request);
-
-                // 传递所有请求头,防止部分丢失
-                //此处也可以只传递认证的header
-                //requestTemplate.header("Authorization", request.getHeader("Authorization"));
-                for (Map.Entry<String, String> entry : headers.entrySet()) {
-                    requestTemplate.header(entry.getKey(), entry.getValue());
-                }
+//                Map<String, String> headers = this.getRequestHeaders(request);
+//
+//                // 传递所有请求头,防止部分丢失
+//                //此处也可以只传递认证的header
+//                //requestTemplate.header("Authorization", request.getHeader("Authorization"));
+//                for (Map.Entry<String, String> entry : headers.entrySet()) {
+//                    requestTemplate.header(entry.getKey(), entry.getValue());
+//                }
 
                 // 微服务之间传递的唯一标识,区分大小写所以通过httpServletRequest获取
                 if (request.getHeader(HeaderIgnoreAutoPackageConstant.FEIGN_TAG) == null) {
