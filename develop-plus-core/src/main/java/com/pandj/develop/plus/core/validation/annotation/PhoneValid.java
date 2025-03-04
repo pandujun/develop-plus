77ad95053b53f1e7e3cfd72959bcff0619dd48a4
pandujun <1355445296@qq.com>
2025-03-04 11:26:51 +0800

    feat(core): 添加手机号校验枚举和规则

---
+++ develop-plus-core/src/main/java/com/pandj/develop/plus/core/validation/annotation/PhoneValid.java
@@ -0,0 +1,38 @@
+package com.pandj.develop.plus.core.validation.annotation;
+
+import com.pandj.develop.plus.core.validation.validator.PhoneValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = PhoneValidator.class)
+@Documented
+public @interface PhoneValid {
+    /**
+     * 校验分组（可选）
+     */
+    Class<?>[] groups() default {};
+
+    /**
+     * 负载信息（可选）
+     */
+    Class<? extends Payload>[] payload() default {};
+
+
+    /**
+     * 错误提示消息（必填）
+     */
+    String message() default "手机号码格式无效";
+
+    /**
+     * 是否可以为空（默认为false）
+     */
+    boolean canNull() default false;
+}
